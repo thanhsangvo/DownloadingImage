912ef9b16c637fce3f6e134cf54d3fcd1a936678
thanhsangvo <pl.thanhsang0710@gmail.com>
2020-07-02 17:12:34 +0700

    Initial Commit

---
+++ DownloadingImage/DownloadingImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadingImageApp.swift
+//  DownloadingImage
+//
+//  Created by Võ Thanh Sang on 7/2/20.
+//
+
+import SwiftUI
+
+@main
+struct DownloadingImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
